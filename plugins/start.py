1de2d4226495189c1c30b114f988d53ef40adb84
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2024-07-27 20:42:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -214,7 +214,7 @@ async def not_joined(client: Client, message: Message):
             InlineKeyboardButton(text="✨ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✨", url=client.invitelink2),
         ],
         [
-            InlineKeyboardButton(text="✨ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✨", url=client.invitelink4),
+            InlineKeyboardButton(text="✨ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✨", url=client.invitelink3),
         ]
     ]
     try:
