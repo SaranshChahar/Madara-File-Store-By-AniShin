7f3cc02b154ae9185822c5a25fa0441f9b2dc9fb
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2024-07-30 21:08:46 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -32,3 +32,39 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+# Admins Database
+async def present_admin(user_id: int):
+    found = await admin_data.find_one({'_id': user_id})
+    return bool(found)
+
+
+async def add_admin(user_id: int):
+    user = new_user(user_id)
+    await admin_data.insert_one(user)
+    ADMINS.append(int(user_id))
+    return
+
+async def del_admin(user_id: int):
+    await admin_data.delete_one({'_id': user_id})
+    ADMINS.remove(int(user_id))
+    return
+
+async def full_adminbase():
+    user_docs = admin_data.find()
+    user_ids = [int(doc['_id']) async for doc in user_docs]
+    return user_ids
+
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+
+    return user_ids
+
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
